4ea1d273294564510ff1fe05a325d0471650e9d4
DrakYo Stay <nitus15@yahoo.com>
2023-05-19 21:05:53 +0300

    added new things

--- Fintess/authentication/models.py
+++ Fintess/authentication/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class WeightEntry(models.Model):
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
--- Fintess/authentication/urls.py
+++ Fintess/authentication/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('login/weight_track', views.weight_track, name='weight_track'),
+    path('update_weight', views.update_weight, name='update_weight'),
 ]
